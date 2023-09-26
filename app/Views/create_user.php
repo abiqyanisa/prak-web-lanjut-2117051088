8b20e3fc767967369127e073f73315264e764720
abiqyanisa <abiqyanisa@gmail.com>
2023-09-26 15:54:04 +0700

    Create Database, Model, and Seeder

--- app/Views/create_user.php
+++ app/Views/create_user.php
@@ -12,10 +12,22 @@
         <label for="exampleInputNama" class="form-label">Nama</label>
         <input type="text" class="form-control" id="input-nama" name="nama" aria-describedby="emailHelp">
     </div>
+
     <div class="mb-3">
-        <label for="exampleInputKelas" class="form-label">Kelas</label>
-        <input type="text" class="form-control" id="input-kelas" name="kelas" aria-describedby="emailHelp">
+    <label for="exampleInputKelas" class="form-label">Kelas</label>
+        <select name="kelas" id="kelas" class="form-select">
+            <?php
+            foreach ($kelas as $item) {
+            ?>
+                <option value="<?= $item['id'] ?>">
+                    <?= $item['nama_kelas'] ?>
+                </option>
+            <?php
+            }
+            ?>
+        </select>
     </div>
+    
     <div class="mb-3">
         <label for="exampleInputNPM" class="form-label">NPM</label>
         <input type="text" class="form-control" id="input-npm" name="npm" aria-describedby="emailHelp">
